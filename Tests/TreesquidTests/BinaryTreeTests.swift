661151b9c72e17ad3e48578969164e1dd9221954
Joachim Baran <joachim.baran@gmail.com>
2021-07-16 23:36:26 -0400

    Additional unit tests for binary trees.

--- Tests/TreesquidTests/BinaryTreeTests.swift
+++ Tests/TreesquidTests/BinaryTreeTests.swift
@@ -3,7 +3,7 @@ import XCTest
 @testable import Treesquid
 
 final class BinaryTreeTests: XCTestCase {
-    static let treeFullThreeLevels = BinaryTree()
+    static let treeFullThreeLevels = BinaryTree<String>()
         .insert(node: BinaryTreeNode(value: "root1"))
         .insert(node: BinaryTreeNode(value: "l2"))
         .insert(node: BinaryTreeNode(value: "r2"))
@@ -11,6 +11,10 @@ final class BinaryTreeTests: XCTestCase {
         .insert(node: BinaryTreeNode(value: "lr3"))
         .insert(node: BinaryTreeNode(value: "rl4"))
         .insert(node: BinaryTreeNode(value: "rr4"))
+    static let treeTwoLevels = BinaryTree<Int>()
+        .insert(node: try! BinaryTreeNode(value: 0)
+                    .append(BinaryTreeNode(value: 1))
+                    .append(BinaryTreeNode(value: 2)))
     
     func testEmptyTreeIsEmpty() {
         let emptyTree = BinaryTree<Any>()
@@ -48,5 +52,46 @@ final class BinaryTreeTests: XCTestCase {
             XCTAssert(levels[level].count == Int(pow(2.0, Double(level))),
                       "Full tree of three levels' level has an incorrect number of nodes.")
         }
+    }
+    
+    func testTreeTwoLevelsDepth() {
+        XCTAssert(BinaryTreeTests.treeTwoLevels.depth() == 2,
+                  "Tree of two levels has incorrect depth.")
+    }
+    
+    func testTreeTwoLevelsBreadth() {
+        XCTAssert(BinaryTreeTests.treeTwoLevels.breadth() == 2,
+                  "Tree of two levels has incorrect breadth.")
+    }
+    
+    func testTreeTwoLevelsContents() {
+        guard let rootNode = BinaryTreeTests.treeTwoLevels.root else {
+            XCTFail("Root node is nil.")
+            return
+        }
+        XCTAssert(rootNode.value == 0,
+                  "Root node's value is incorrect. Expected: 0")
+        for index in 0...1 {
+            XCTAssert(rootNode[index] != nil,
+                      "Root node's child at \(index) is nil.")
+            let expectedValue = index + 1
+            XCTAssert(rootNode[index]!.value == expectedValue,
+                      "Root node's child at \(index) has an incorrect value. Expected: \(expectedValue)")
+        }
+    }
+    
+    func testTreeTwoLevelsIndexEquivalency() {
+        guard let rootNode = BinaryTreeTests.treeTwoLevels.root as! BinaryTreeNode<Int>? else {
+            XCTFail("Root node is nil.")
+            return
+        }
+        XCTAssert(rootNode[0] === rootNode[BinaryTreeNode<Int>.Child.left.rawValue],
+                  "Subscript: index 0 is not equivalent to Child.left.rawValue.")
+        XCTAssert(rootNode[1] === rootNode[BinaryTreeNode<Int>.Child.right.rawValue],
+                  "Subscript: index 0 is not equivalent to Child.right.rawValue.")
+        XCTAssert(rootNode[BinaryTreeNode.Child.left] === rootNode[BinaryTreeNode<Int>.Child.left.rawValue],
+                  "Subscript: index 0 is not equivalent to Child.left.rawValue.")
+        XCTAssert(rootNode[BinaryTreeNode.Child.right] === rootNode[BinaryTreeNode<Int>.Child.right.rawValue],
+                  "Subscript: index 0 is not equivalent to Child.right.rawValue.")
     }
 }
