a74cf223fa16a4ae065e63e25c8984c98e79f3e2
Joachim Baran <joachim.baran@gmail.com>
2021-08-01 20:48:00 -0400

    Added an indent to the output of a tree traversal. #8

--- Sources/Treesquid/TreeUtils.swift
+++ Sources/Treesquid/TreeUtils.swift
@@ -5,12 +5,14 @@ public func traverse(tree: RedBlackTree<Int, Any>) {
         print("empty tree")
         return
     }
+    let detailsWidth = 3 // Note: Does not include separating space.
+    let indentWidth = detailsWidth + 1 // Number of spaces to indent every line.
     let depth = tree.depth()
     let maxNodesAtBottom = pow(2.0, Double(depth) - 1.0)
-    let detailsWidth = 3 // Note: Does not include separating space.
     let levelStack = levels(of: tree, fillWithVoidNodes: true)
     for levelIndex in 0..<levelStack.count {
         let maxNodesInLevel = pow(2.0, Double(levelIndex))
+        print(String(repeating: " ", count: indentWidth), terminator: "")
         print(String(repeating: " ",
                      count: Int(Double(detailsWidth + 1) / 2.0 * (maxNodesAtBottom - maxNodesInLevel))), terminator: "")
         for nodeIndex in 0..<Int(maxNodesInLevel) {
