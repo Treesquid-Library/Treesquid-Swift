00ca253585b71b474ced4dc96e886aed6d64f1a2
Joachim Baran <joachim.baran@gmail.com>
2021-08-05 21:48:40 -0400

    Added Red/Black Tree unit tests for insertion cases.

--- Tests/TreesquidTests/RedBlackTreeTests.swift
+++ Tests/TreesquidTests/RedBlackTreeTests.swift
@@ -1,8 +1,239 @@
-//
-//  File.swift
-//  
-//
-//  Created by Joachim Baran on 2021-07-25.
-//
-
 import Foundation
+import XCTest
+@testable import Treesquid
+
+final class RedBlackTreeTests: XCTestCase {
+    // Example from:
+    // http://www.btechsmartclass.com/data_structures/red-black-trees.html
+    // (July 30th, 2021)
+    static let treeBtechsmartclass = RedBlackTree<Int, Any>()
+        .insert(node: RedBlackTreeNode(key: 8))
+        .insert(node: RedBlackTreeNode(key: 18))
+        .insert(node: RedBlackTreeNode(key: 5))
+        .insert(node: RedBlackTreeNode(key: 15))
+        .insert(node: RedBlackTreeNode(key: 17))
+        .insert(node: RedBlackTreeNode(key: 25))
+        .insert(node: RedBlackTreeNode(key: 40))
+        .insert(node: RedBlackTreeNode(key: 80))
+
+    func validate(tree: RedBlackTree<Int, Any>, is reference: String) {
+        XCTAssert(traverse(tree: tree) == reference, "Trees differ. Expected:\n\(reference)")
+    }
+    
+    // Example from:
+    // https://60devs.com/a/data-structures/red-black-tree/
+    // (August 4th, 2021)
+    func test60devsTree() {
+        let tree60devs = RedBlackTree<Int, Any>()
+            .insert(node: RedBlackTreeNode(key: 8))
+        validate(tree: tree60devs.insert(node: RedBlackTreeNode(key: 8)), is: """
+    08◻︎
+""")
+        validate(tree: tree60devs.insert(node: RedBlackTreeNode(key: 17)), is: """
+      08◼︎
+    --- 17◻︎
+""")
+        validate(tree: tree60devs.insert(node: RedBlackTreeNode(key: 1)), is: """
+      08◼︎
+    01◻︎ 17◻︎
+""")
+        validate(tree: tree60devs.insert(node: RedBlackTreeNode(key: 11)), is: """
+          08◻︎
+        01◼︎ 17◼︎
+    --- --- 11◻︎ ---
+""")
+        validate(tree: tree60devs.insert(node: RedBlackTreeNode(key: 15)), is: """
+          08◻︎
+        01◼︎ 15◼︎
+    --- --- 11◻︎ 17◻︎
+""")
+        validate(tree: tree60devs.insert(node: RedBlackTreeNode(key: 25)), is: """
+                  08◼︎
+                01◼︎ 15◻︎
+            --- --- 11◼︎ 17◼︎
+    --- --- --- --- --- --- --- 25◻︎
+""")
+        validate(tree: tree60devs.insert(node: RedBlackTreeNode(key: 6)), is: """
+                  08◼︎
+                01◼︎ 15◻︎
+            --- 06◻︎ 11◼︎ 17◼︎
+    --- --- --- --- --- --- --- 25◻︎
+""")
+        validate(tree: tree60devs.insert(node: RedBlackTreeNode(key: 22)), is: """
+                  08◼︎
+                01◼︎ 15◻︎
+            --- 06◻︎ 11◼︎ 22◼︎
+    --- --- --- --- --- --- 17◻︎ 25◻︎
+""")
+        validate(tree: tree60devs.insert(node: RedBlackTreeNode(key: 27)), is: """
+                  15◼︎
+                08◻︎ 22◻︎
+            01◼︎ 11◼︎ 17◼︎ 25◼︎
+    --- 06◻︎ --- --- --- --- --- 27◻︎
+""")
+        validate(tree: tree60devs.insert(node: RedBlackTreeNode(key: 5)), is: """
+                  15◼︎
+                08◻︎ 22◻︎
+            05◼︎ 11◼︎ 17◼︎ 25◼︎
+    01◻︎ 06◻︎ --- --- --- --- --- 27◻︎
+""")
+        validate(tree: tree60devs.insert(node: RedBlackTreeNode(key: 13)), is: """
+                  15◼︎
+                08◻︎ 22◻︎
+            05◼︎ 11◼︎ 17◼︎ 25◼︎
+    01◻︎ 06◻︎ --- 13◻︎ --- --- --- 27◻︎
+""")
+    }
+    
+    // Example from:
+    // http://software.ucv.ro/~mburicea/lab8ASD.pdf
+    // (July 31st, 2021)
+    func testUCVmburiceaTree() {
+        // From "1.2.4 Sample operations in a Red-Black Tree" with the
+        // exception that Treesquid trees have a red root.
+        let treeUCVmburicea = RedBlackTree<Int, Any>()
+            .insert(node: RedBlackTreeNode(key: 15))
+            .insert(node: RedBlackTreeNode(key: 0))
+            .insert(node: RedBlackTreeNode(key: 20))
+        validate(tree: treeUCVmburicea.insert(node: RedBlackTreeNode(key: 3)), is: """
+          15◻︎
+        00◼︎ 20◼︎
+    --- 03◻︎ --- ---
+""")
+        validate(tree: treeUCVmburicea.insert(node: RedBlackTreeNode(key: 13)), is: """
+          15◻︎
+        03◼︎ 20◼︎
+    00◻︎ 13◻︎ --- ---
+""")
+        validate(tree: treeUCVmburicea.insert(node: RedBlackTreeNode(key: 14)), is: """
+                  15◼︎
+                03◻︎ 20◼︎
+            00◼︎ 13◼︎ --- ---
+    --- --- --- 14◻︎ --- --- --- ---
+""")
+        validate(tree: treeUCVmburicea.insert(node: RedBlackTreeNode(key: 45)), is: """
+                  15◼︎
+                03◻︎ 20◼︎
+            00◼︎ 13◼︎ --- 45◻︎
+    --- --- --- 14◻︎ --- --- --- ---
+""")
+        validate(tree: treeUCVmburicea.insert(node: RedBlackTreeNode(key: 34)), is: """
+                  15◼︎
+                03◻︎ 34◼︎
+            00◼︎ 13◼︎ 20◻︎ 45◻︎
+    --- --- --- 14◻︎ --- --- --- ---
+""")
+    }
+    
+    // Example from:
+    // http://homepages.math.uic.edu/~jan/mcs360/red_black_trees.pdf
+    // (July 31st, 2021)
+    func testA() {
+        let treeUICjan = RedBlackTree<Int, Any>()
+        validate(tree: treeUICjan.insert(node: RedBlackTreeNode(key: 48)), is: """
+    48◻︎
+""")
+        validate(tree: treeUICjan.insert(node: RedBlackTreeNode(key: 84)), is: """
+      48◼︎
+    --- 84◻︎
+""")
+        validate(tree: treeUICjan.insert(node: RedBlackTreeNode(key: 43)), is: """
+      48◼︎
+    43◻︎ 84◻︎
+""")
+        validate(tree: treeUICjan.insert(node: RedBlackTreeNode(key: 91)), is: """
+          48◻︎
+        43◼︎ 84◼︎
+    --- --- --- 91◻︎
+""")
+        validate(tree: treeUICjan.insert(node: RedBlackTreeNode(key: 38)), is: """
+          48◻︎
+        43◼︎ 84◼︎
+    38◻︎ --- --- 91◻︎
+""")
+        validate(tree: treeUICjan.insert(node: RedBlackTreeNode(key: 79)), is: """
+          48◻︎
+        43◼︎ 84◼︎
+    38◻︎ --- 79◻︎ 91◻︎
+""")
+        validate(tree: treeUICjan.insert(node: RedBlackTreeNode(key: 63)), is: """
+                  48◼︎
+                43◼︎ 84◻︎
+            38◻︎ --- 79◼︎ 91◼︎
+    --- --- --- --- 63◻︎ --- --- ---
+""")
+        validate(tree: treeUICjan.insert(node: RedBlackTreeNode(key: 59)), is: """
+                  48◼︎
+                43◼︎ 84◻︎
+            38◻︎ --- 63◼︎ 91◼︎
+    --- --- --- --- 59◻︎ 79◻︎ --- ---
+""")
+        validate(tree: treeUICjan.insert(node: RedBlackTreeNode(key: 49)), is: """
+                  63◼︎
+                48◻︎ 84◻︎
+            43◼︎ 59◼︎ 79◼︎ 91◼︎
+    38◻︎ --- 49◻︎ --- --- --- --- ---
+""")
+        validate(tree: treeUICjan.insert(node: RedBlackTreeNode(key: 98)), is: """
+                  63◼︎
+                48◻︎ 84◻︎
+            43◼︎ 59◼︎ 79◼︎ 91◼︎
+    38◻︎ --- 49◻︎ --- --- --- --- 98◻︎
+""")
+    }
+
+    func testBtechsmartclassTree() {
+        validate(tree: RedBlackTreeTests.treeBtechsmartclass, is: """
+                  17◼︎
+                08◻︎ 25◻︎
+            05◼︎ 15◼︎ 18◼︎ 40◼︎
+    --- --- --- --- --- --- --- 80◻︎
+""")
+    }
+    
+    // Example from:
+    // https://www.csee.umbc.edu/courses/undergraduate/341/spring04/hood/notes/red_black/
+    // (August 2nd, 2021)
+    func testUMBCTree() {
+        let treeUMBCTree = RedBlackTree<Int, Any>()
+        validate(tree: treeUMBCTree.insert(node: RedBlackTreeNode(key: 2)), is: """
+    02◻︎
+""")
+        validate(tree: treeUMBCTree.insert(node: RedBlackTreeNode(key: 1)), is: """
+      02◼︎
+    01◻︎ ---
+""")
+        validate(tree: treeUMBCTree.insert(node: RedBlackTreeNode(key: 4)), is: """
+      02◼︎
+    01◻︎ 04◻︎
+""")
+        validate(tree: treeUMBCTree.insert(node: RedBlackTreeNode(key: 5)), is: """
+          02◻︎
+        01◼︎ 04◼︎
+    --- --- --- 05◻︎
+""")
+        validate(tree: treeUMBCTree.insert(node: RedBlackTreeNode(key: 9)), is: """
+          02◻︎
+        01◼︎ 05◼︎
+    --- --- 04◻︎ 09◻︎
+""")
+        validate(tree: treeUMBCTree.insert(node: RedBlackTreeNode(key: 3)), is: """
+                  02◼︎
+                01◼︎ 05◻︎
+            --- --- 04◼︎ 09◼︎
+    --- --- --- --- 03◻︎ --- --- ---
+""")
+        validate(tree: treeUMBCTree.insert(node: RedBlackTreeNode(key: 6)), is: """
+                  02◼︎
+                01◼︎ 05◻︎
+            --- --- 04◼︎ 09◼︎
+    --- --- --- --- 03◻︎ --- 06◻︎ ---
+""")
+        validate(tree: treeUMBCTree.insert(node: RedBlackTreeNode(key: 7)), is: """
+                  02◼︎
+                01◼︎ 05◻︎
+            --- --- 04◼︎ 07◼︎
+    --- --- --- --- 03◻︎ --- 06◻︎ 09◻︎
+""")
+    }
+}
